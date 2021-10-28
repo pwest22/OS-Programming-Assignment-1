dc208b432d756831da9a3fa1d2950565790a1a53
pwest22 <90290517+pwest22@users.noreply.github.com>
2021-10-28 12:39:09 -0400

    Update consumer.cpp

--- consumer.cpp
+++ consumer.cpp
@@ -1,28 +1,30 @@
-//include the proper libraries
-//include producer-consumer.hpp
+#include "producer-consumer.hpp"
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+using namespace std;
 
 int main() {
-  //start by declaring shared memory, link the memory to the pointer of our array
-  do {
-        //lock the semaphores so only consumer.cpp can access them
-        if (//empty (our semaphore tracking how many spaces in the table are empty) = 0) {
-        //checks to see if the table is no longer empty, meaning the consumer waits until the table is
-        //completely full before it consumes
-            //table (our pointer to our array) = 0;  an arbitrary value, does not really matter
-            //--table;  deincrements the pointer to previous point in array
-            //--filled;  deincrements the semaphore so we know one less piece of the table is filled
-            //++empty;  increments the semaphore so we know one more piece of the table is empty
-            //for testing purposes we can have a cout statement telling us that the table was filled
-            //cout << "Producer has added an item to the table." << endl;
-        }
-        //++"iterator"/"overall";  increments our overarching semaphore so we can track how many times
-        //the code has been run
-        //unlock the semaphores since consumer.cpp isnt currently using them
-  } while (//"iterator"/"overall" is less than an arbitrary amount, for sake of testing; in a real OS,
-    //it would do the code while "true", meaning it would continue forever until not needed anymore
-  //finally delete/deallocate/unlink/remove the shared memory; will only execute this step once the
-  //while loop has completed the set number of times
-  //one logistical error I can think of with this code is that you may want the consumer to continue to
-  //run in order to empty out the table fully; to do this you would just have this consumer program
-  //run a few extra times to clear out whats left in the table
+        int shmid = shmget(101, 1024, 0665|IPC_CREAT);
+        shmat(shmid, table, 0);
+        int i = 0;
+        do {
+                std::cout << "inside do loop" << std::endl;
+                sem_wait(&filled);
+                sem_wait(&mutex);
+                std::cout << "setting table to 0" << std::endl;
+                *table = 0;
+                std::cout << "deincrementing table" << std::endl;
+                --table;
+                std::cout << "Consumer has removed an item from the table." << std::endl;
+                ++i;
+                sem_post(&mutex);
+                sem_post(&empty);
+        } while (i < 10);
+int non1 = shmdt(table);
+int non2 = sem_destroy(&filled);
+int non3 = sem_destroy(&empty);
+int non4 = sem_destroy(&mutex);
 }
